4ec2314c50f62fc41df3ab289ea01a4409334c0b
Safiyat Reza <md.reza@snapdeal.com>
2015-12-15 20:01:25 +0530

    Removed ceph-deploy

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get update
 
-sudo apt-get install -y --force-yes calamari-clients calamari-server diamond
+sudo apt-get install -y --force-yes calamari-clients calamari-server diamond python-pip
 
 
 sudo pip install --upgrade ceph-deploy
@@ -16,4 +16,4 @@ sudo calamari-ctl initialize --admin-username ubuntu --admin-password ubuntu --a
 ## On first master.
 ##
 
-sudo ceph-deploy --cluster sdcluster --username ubuntu calamari connect --master `ifconfig em1|grep 'inet addr:'|cut -d: -f2|awk '{ print $1}'|tr -d ' '` 10.41.0.93 10.41.0.94
+# sudo ceph-deploy --cluster sdcluster --username ubuntu calamari connect --master `ifconfig em1|grep 'inet addr:'|cut -d: -f2|awk '{ print $1}'|tr -d ' '` 10.41.0.93 10.41.0.94
